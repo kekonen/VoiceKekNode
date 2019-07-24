3fa969338f5383fcea261e4b2a3572e361e70655
kekonen <daniil.naumetc@gmail.com>
2019-07-24 15:15:31 +0200

    seq

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const botToken = process.env.BOT_TOKEN;
-const [hostname, user, password, database] = process.env.DB_HOST.split(',');
+// const [hostname, user, password, database] = process.env.DB_HOST.split(',');
 
 
 
 
 async function init()
 {
-    const db = new DB({ hostname, user, password, database });
+    const db = new DB(/*{ hostname, user, password, database }*/);
 
     const bot = new Bot(botToken);
     await bot.setup(db);
